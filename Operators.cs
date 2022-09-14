811a49af8e2bee27c4762fbee91787e593f15882
Vira Murray <vira.avramenko@gmail.com>
2022-09-14 13:05:25 -0400

    Operators - exercise 1

---
+++ Operators.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorExercise
+{
+    internal class Operators
+    {
+
+        public static int OperatorStringAdd(int a, int b)
+        {
+            //Exercise 1:
+            return a += b;
+
+        }
+        public static int OperatorStringSubstarct(int a, int b)
+        {
+            return a -= b;
+
+        }
+        public static int OperatorStringDivide(int a, int b)
+        {
+            return a /= b;
+
+        }
+
+        public static int OperatorStringMultiply(int a, int b)
+        {
+            return a *= b;
+
+        }
+        public static int OperatorStringModulus(int a, int b)
+        {
+            return a %= b;
+        }
+
+        //public static int opStr()
+        //{
+        //    if (a == 17 && b == 4)
+        //    {
+        //        return stringNum;
+        //    }
+        //}
+
+    }
+}
+
+//int a = 17;
+//int b = 4;
+//int quotient = b;
+//int remainder = a % b;
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Exercise 1:
+
+//int remainder = a % b;
+using OperatorExercise;
+int a = 17;
+int b = 4;
+
+Console.WriteLine($"sum: {Operators.OperatorStringAdd(a, b)}");
+Console.WriteLine($"Substraction: {Operators.OperatorStringSubstarct(a, b)}");
+Console.WriteLine($"Division: {Operators.OperatorStringDivide(a, b)}");
+Console.WriteLine($"Modulus: {Operators.OperatorStringModulus(a, b)}");
+Console.WriteLine($"Multiply: {Operators.OperatorStringMultiply(a, b)}");
+Console.WriteLine($"Quotient:");
+int quotient = Operators.OperatorStringDivide(a, b);
+Console.WriteLine($"{quotient}");
+
+int remainder = Operators.OperatorStringModulus(a, b);
+Console.WriteLine($"Remainder:");
+Console.WriteLine($"{remainder}");
+
+
+string opString = (a == 17 && b == 4) ? $"{a} / {b} is {quotient} remainder {1}" : "Brush up on your math";
+
+Console.WriteLine(opString);
+
+
+
+
+
+
+
